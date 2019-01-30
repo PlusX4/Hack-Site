bafd73dece55847480138825bf4bb7f8684466b7
ProMast3r X4 <47147208+promast3rx4@users.noreply.github.com>
2019-01-30 23:29:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 clear
+mkdir .h
 cd $HOME/H-Site/.h/
 git clone https://github.com/sqlmapproject/sqlmap
 clear
 cd sqlmap
 chmod +x sqlmap*
+echo ""
+echo ""
+echo ""
+echo ""
 read -p "Done [ Press Enter ] "
 cd $HOME/H-Site
 ls
