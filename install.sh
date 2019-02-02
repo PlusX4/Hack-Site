2331d84f314f2b0cb485b03ea3afee0fc4621b6d
ProMast3r X4 <47147208+promast3rx4@users.noreply.github.com>
2019-02-02 21:50:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 clear
 mkdir .H-Site
 cd $Home
-cp sqlmap $HOME/Hack-Site/.H-Site
+cp -r sqlmap $HOME/Hack-Site/.H-Site
 cd $HOME/Hack-Site/.H-Site
 git clone https://github.com/sqlmapproject/sqlmap
 clear
